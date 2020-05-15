c7cd6e643afeb639d0502c98aebfcbbc61a9ff9e
yuanlinzhu <1240291523@qq.com>
2020-05-15 10:19:20 +0800

    client create

---
+++ src/main/java/com/tunnel/tunnel/Service/EchoClientHandler.java
@@ -0,0 +1,29 @@
+package com.tunnel.tunnel.Service;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println("Client received: " + msg.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", //2
+                CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) { //4
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
